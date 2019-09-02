4f9d87fb045ccf6d8462dc4585094bdf99d30602
Miguel López <miguellopez@vitalware.com>
2019-09-02 13:21:34 -0500

    update html to use tailwind classes

--- components/Nav.js
+++ components/Nav.js
@@ -2,37 +2,18 @@ import Link from 'next/link';
 
 const Nav = () => (
   <div>
-    <ul>
-      <li>
+    <ul className="list-none flex bg-gray-600">
+      <li className="py-2 mr-4">
         <Link href="/">
-          <a>Home</a>
+          <a className="text-white text-xl no-underline">Home</a>
         </Link>
       </li>
-      <li>
+      <li className="py-2 mr-4">
         <Link href="/about">
-          <a>About Us</a>
+          <a className="text-white text-xl no-underline">About Us</a>
         </Link>
       </li>
     </ul>
-    {/* TODO: Move this */}
-    <style jsx>
-      {`
-        ul {
-          background: #333;
-          list-style: none;
-          display: flex;
-        }
-        ul li {
-          padding: .5rem 0;
-          margin-right: 1rem;
-        }
-        ul li a {
-          font-size: 1.2rem;
-          color: white;
-          text-decoration; none;
-        }
-      `}
-    </style>
   </div>
 );
 
