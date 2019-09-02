069d8733e0685b4ba0e1c2eca05dffbea900a99c
Miguel López <miguellopez@vitalware.com>
2019-09-01 20:13:10 -0500

    index/about pages

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import { Nav } from '../components';
+
+const About = () => (
+  <div>
+    <Nav />
+    <p>About Us</p>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { Nav } from '../components';
+import '../styles/index.css';
+
+const Index = () => (
+  // <div>
+  //   <Nav />
+  //   <p>index</p>
+  // </div>
+  <div className="p-4 shadow rounded bg-white">
+    <h1 className="text-purple leading-normal">Next.js</h1>
+    <p className="text-grey-dark">with Tailwind CSS</p>
+  </div>
+);
+
+export default Index;
