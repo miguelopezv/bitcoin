d5717b6e2c8b3778aa2d5dd8dfda67941aa1168d
Miguel López <miguellopez@vitalware.com>
2019-09-02 16:19:30 -0500

    markup update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import { Nav } from '../components';
-import '../styles/index.css';
+import { Layout } from '../components';
 
 const Index = () => (
-  <div>
-    <Nav />
-    <p>index</p>
-  </div>
+  <Layout>
+    <div className="flex">
+      <div className="w-full">
+        <h2>Bitcoin price</h2>
+      </div>
+    </div>
+    <div className="flex">
+      <div className="w-2/3">
+        <h2>News</h2>
+      </div>
+      <div className="w-1/3">
+        <h2>Upcoming events</h2>
+      </div>
+    </div>
+  </Layout>
 );
 
 export default Index;
