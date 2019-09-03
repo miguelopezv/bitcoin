d38aba6b3e583d2ffe4673db6ee7794c05d298ff
Miguel López <miguellopez@vitalware.com>
2019-09-03 17:10:37 -0500

    add button to site

--- components/Article.js
+++ components/Article.js
@@ -12,10 +12,17 @@ const Article = ({ article }) => {
           <div className="font-bold text-xl mb-2">{title}</div>
           <p className="text-gray-700 text-base">{description}</p>
         </div>
-        <div className="px-6 py-4">
+        <div className="flex justify-between items-center px-6 py-4">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
             {source.name}
           </span>
+          <a
+            href={url}
+            target="_blank"
+            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          >
+            Go to site
+          </a>
         </div>
       </div>
     </div>
